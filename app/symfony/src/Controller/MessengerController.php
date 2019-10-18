92ad3cb38de696d743f753cb6fdcb82720c95750
Jonathan Kablan <jonathan.kablan@gmail.com>
2019-10-18 13:44:34 +0200

    config docker redis image

--- app/symfony/src/Controller/MessengerController.php
+++ app/symfony/src/Controller/MessengerController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Message\SmsNotification;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +12,15 @@ class MessengerController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(MessageBusInterface $bus)
     {
+
+         // will cause the SmsNotificationHandler to be called
+         $bus->dispatch(new SmsNotification('Look! I created a message!'));
+
+         // or use the shortcut
+         //$this->dispatchMessage(new SmsNotification('Look! I created a message!'));
+
         return $this->render('messenger/index.html.twig', [
             'controller_name' => 'Messenger handler',
         ]);
